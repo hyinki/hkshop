b932fdd0707fc9a88e0e2b5204fc2dbfa82761bf
hyinki <113676721+hyinki@users.noreply.github.com>
2024-07-03 15:17:55 +0800

    token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from '../middleware/asyncHandler.js';
 import User from '../models/UserModel.js';
+import jwt from 'jsonwebtoken';
 
 
 
@@ -8,6 +9,16 @@ const authUser = asyncHandler(async (req, res) => {
 
     const user = await User.findOne({ email });
     if (user && (await user.matchPassword(password))) {
+        const token = jwt.sign({ userId: user._id}, process.env.JWT_SECRET, {
+            expiresIn: '30d'
+        })
+
+        res.cookie('jwt', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV !== 'development',
+            sameSite: 'Strict',
+            maxAge: 30*24*60*60*1000
+        })
         //generateToken(res, user._id);
     
         res.json({
