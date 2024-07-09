9f5bf39f38701f21cb4cc5f481c041cc9dc60f89
hyinki <113676721+hyinki@users.noreply.github.com>
2024-07-09 21:45:44 +0800

    admin

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,12 +3,13 @@ const router = express.Router();
 import {createProduct, 
     getProducts, 
     getProductsById,
-    updateProduct
+    updateProduct,
+    deleteProduct
 } from '../controllers/productController.js'
 import {protect, admin} from '../middleware/authMiddleware.js'
 
 router.route('/').get(getProducts).post(protect, admin, createProduct);
-router.route('/:id').get(getProductsById).put(protect, admin, updateProduct);
+router.route('/:id').get(getProductsById).put(protect, admin, updateProduct).delete(protect, admin, deleteProduct);
 
 
 
