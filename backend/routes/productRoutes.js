2572d63d5afc794ad7043464fadce385d963c438
hyinki <113676721+hyinki@users.noreply.github.com>
2024-07-10 14:07:26 +0800

    r

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,13 +4,14 @@ import {createProduct,
     getProducts, 
     getProductsById,
     updateProduct,
-    deleteProduct
+    deleteProduct,
+    createProductReview
 } from '../controllers/productController.js'
 import {protect, admin} from '../middleware/authMiddleware.js'
 
 router.route('/').get(getProducts).post(protect, admin, createProduct);
 router.route('/:id').get(getProductsById).put(protect, admin, updateProduct).delete(protect, admin, deleteProduct);
-
+router.route('/:id/reviews').post(protect, createProductReview)
 
 
 export default router;
