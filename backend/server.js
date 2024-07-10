16e583ac5711b2988bb9df0c7932d49a8ac55898
hyinki <113676721+hyinki@users.noreply.github.com>
2024-07-10 17:31:03 +0800

    d

--- backend/server.js
+++ backend/server.js
@@ -21,9 +21,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(cookieParser())
 
 
-app.get('/', (req, res)=> {
-    res.send('api running');
-})
+
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
@@ -40,6 +38,17 @@ app.get('/api/config/paypal', (req, res)=> res.send({clientId: process.env.PAYPA
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+    app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    );
+}else{
+    app.get('/', (req, res)=> {
+        res.send('api running');
+    })
+}
 
 app.use(notFound);
 app.use(errorHandler);
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,19 +1,61 @@
-import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap';
+// import React from 'react'
+// import { Container, Row, Col } from 'react-bootstrap';
+
+// const Footer = () => {
+//     const currentYear = new Date().getFullYear();
+//   return (
+//     <footer>
+//       <Container>
+//         <Row>
+//           <Col className='text-center py-3'>
+//             <p>hkshop &copy; {currentYear}</p>
+//           </Col>
+//         </Row>
+//       </Container>
+//     </footer>
+//   )
+// }
+
+// export default Footer
+
+// src/components/Footer.js
+import React from 'react';
+import { Container, Row, Col, Nav } from 'react-bootstrap';
+import '../assets/styles/index.css'
 
 const Footer = () => {
-    const currentYear = new Date().getFullYear();
   return (
-    <footer>
+    <footer className="header text-white mt-5 py-3">
       <Container>
-        <Row>
-          <Col className='text-center py-3'>
-            <p>hkshop &copy; {currentYear}</p>
+        <Row className='mt-5'>
+         <Col md={5}></Col>
+          {/* <Col md={4}>
+            <h5>Quick Links</h5>
+            <Nav className="flex-column">
+              <Nav.Link href="#home" className="text-white">Home</Nav.Link>
+              <Nav.Link href="#services" className="text-white">Services</Nav.Link>
+              <Nav.Link href="#contact" className="text-white">Contact</Nav.Link>
+            </Nav>
+          </Col> */}
+          <Col md={4}>
+            <h5>Contact Me</h5>
+            <p>Email: pang_hyin_ki@yahoo.com.sg</p>
+            <p>Phone: +123 456 7890</p>
           </Col>
+          <Col md={2}></Col>
+        </Row>
+      </Container>
+
+      <Container>
+         <Row>
+           <Col className='text-center py-3'>
+          <p>hkshop &copy; 2024</p>
+           </Col>
         </Row>
       </Container>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
+
