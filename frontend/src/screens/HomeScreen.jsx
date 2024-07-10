8abf11a6f2bef0b956a835c54b2cc361f656f81e
hyinki <113676721+hyinki@users.noreply.github.com>
2024-07-10 13:46:22 +0800

    j

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -6,24 +6,30 @@ import Message from '../components/Message';
 import { useGetProductsQuery } from '../slices/productApiSlice';
 
 const HomeScreen = () => {
-  const { data: products, isLoading, error } = useGetProductsQuery({keyword: '', pageNumber: 1 });
+  const { data, isLoading, error } = useGetProductsQuery({ keyword: '', pageNumber: 1 });
+
+  // Ensure that products is an array, defaulting to an empty array if undefined
+  const products = data ? data.products : [];
 
   return (
     <>
       {isLoading ? (
-        
-        <Loader/>
+        <Loader />
       ) : error ? (
         <Message variant='danger'>{error?.data?.message || error.error}</Message>
       ) : (
         <>
           <h1>Latest Products</h1>
           <Row>
-            {products.map((product) => (
-              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                <Product product={product} />
-              </Col>
-            ))}
+            {Array.isArray(products) && products.length > 0 ? (
+              products.map((product) => (
+                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                  <Product product={product} />
+                </Col>
+              ))
+            ) : (
+              <Message variant='info'>No products found</Message>
+            )}
           </Row>
         </>
       )}
